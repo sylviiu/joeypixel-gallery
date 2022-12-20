47ede1d49363976142b70a494cdde47703ee1f03
Syl <hi@sylviiu.dev>
2022-12-20 11:20:16 -0600

    run tasks in background to be as efficient as possible

--- handlers/8-fileStructure.js
+++ handlers/8-fileStructure.js
@@ -1,78 +1,116 @@
 const fs = require('fs');
-const time = require(`../util/time`)
+const time = require(`../util/time`);
+
+let fileStructure = {};
+
+const readFileStructure = () => new Promise(async res => {
+    console.log(`Reading structure...`)
+    const parseDir = (dir) => {
+        try {
+            const read = typeof dir == `object` ? dir : fs.readdirSync(dir);
+            return {
+                name: dir.split(`/`).slice(-1)[0],
+                location: dir.split(`/`).slice(2).join(`/`),
+                type: fs.existsSync(dir + `/`) ? `directory` : dir.split(`.`).slice(-1)[0],
+                files: read.map(f => parseDir(dir + `/` + f))
+            }
+        } catch(e) {
+            //console.warn(`Couldn't readdir for ${dir} -- ${e}, attempting file`);
+
+            try {
+                let name = dir.split(`/`).slice(-1)[0];
+
+                if(name.toLowerCase().startsWith(`spoiler_`)) name = name.slice(`spoiler_`.length)
+
+                let date = null, datestr = null;
+
+                if(name.startsWith(`VRChat_`) || name.startsWith(`vrchat_`)) {
+                    if(name.split(`_`).length === 4 && !name.includes(`(`) && !name.split(`_`).slice(-1)[0].includes(`x`)) name = name.split(`_`).slice(1).join(`_`)
+                    const day = name.split(`_`).slice(1, 2)[0].split(`-`);
+                    const time = name.split(`_`).slice(2, 3)[0].split(`.`)[0].split(`-`);
+                    datestr = `${day.map(s => s.split(`.`)[0]).join(`-`)}T${time.map(s => s.split(`.`)[0]).join(`:`)}`
+                    date = (new Date(datestr)).getTime()
+                } else if(name[14] == `_`) {
+                    const yr = name.slice(0, 4), month = name.slice(4, 6), day = name.slice(6, 8), h = name.slice(8, 10), m = name.slice(10, 12), s = name.slice(12, 14);
+                    datestr = `${yr}-${month}-${day.split(`.`)[0]}T${h}:${m}:${s.split(`.`)[0]}`
+                    date = (new Date(datestr)).getTime()
+                } else if(name[21] == `_`) {
+                    name = name.slice(21 - 14);
+                    const yr = name.slice(0, 4), month = name.slice(4, 6), day = name.slice(6, 8), h = name.slice(8, 10), m = name.slice(10, 12), s = name.slice(12, 14);
+                    datestr = `${yr}-${month}-${day.split(`.`)[0]}T${h}:${m}:${s.split(`.`)[0]}`
+                    date = (new Date(datestr)).getTime()
+                };
+
+                if(!date) console.log(dir.split(`/`).slice(-1)[0], datestr)
+
+                const fd = fs.openSync(dir), fstat = fs.fstatSync(fd);
+                const ms = date || Object.entries(fstat).filter(o => o[0].endsWith(`Ms`)).map(o => o[1]).sort()[0]
+                const utc = time(ms).utc;
+
+                const cachedImages = fs.readdirSync(`./cache/`)
+
+                const cachedImage = require('../util/getCacheName')(dir.split(`/`).slice(2).join(`/`));
+
+                //console.log(`${dir.split(`/`).slice(-1)[0]} as ${cachedImage} exists? ${cachedImages.indexOf(cachedImage) != -1 ? true : false}`)
+
+                return {
+                    createdAt: { ms, utc },
+                    name: dir.split(`/`).slice(-1)[0],
+                    location: dir.split(`/`).slice(2).join(`/`),
+                    type: dir.split(`/`).slice(-1)[0].split(`.`).slice(-1)[0],
+                    cachedImage: {
+                        file: cachedImage,
+                        exists: cachedImages.indexOf(cachedImage) != -1 ? true : false
+                    }
+                }
+            } catch(e2) {
+                console.warn(`Coudln't read directory OR file:\n- Directory: ${e}\n- File: ${e2}\nReturning null.`);
+                return null;
+            }
+        }
+    }
+    
+    const files = fs.readdirSync(`./files`);
+
+    let f = [];
+
+    for (o of files) {
+        const a = parseDir(`./files/${o}`);
+        f.push(a)
+        await new Promise(r => setTimeout(r, 10))
+    }
+
+    fileStructure = f;
+    res(f)
+});
+
+let currentFileStructure = null
+
+const timer = async () => {
+    while(true) {
+        await new Promise(async r => {
+            currentFileStructure = readFileStructure();
+
+            await currentFileStructure;
+
+            currentFileStructure = null;
+
+            console.log(`fileStructure completed!`)
+
+            setTimeout(r, 60000);
+        })
+    }
+};
+
+timer();
 
 module.exports = {
     path: `/fileStructure`,
     func: async (req, res) => {
-        console.log(`fileStructure requested`);
-
-        const parseDir = (dir) => {
-            try {
-                const read = typeof dir == `object` ? dir : fs.readdirSync(dir);
-                return {
-                    name: dir.split(`/`).slice(-1)[0],
-                    location: dir.split(`/`).slice(2).join(`/`),
-                    type: fs.existsSync(dir + `/`) ? `directory` : dir.split(`.`).slice(-1)[0],
-                    files: read.map(f => parseDir(dir + `/` + f))
-                }
-            } catch(e) {
-                //console.warn(`Couldn't readdir for ${dir} -- ${e}, attempting file`);
-
-                try {
-                    let name = dir.split(`/`).slice(-1)[0];
-
-                    if(name.toLowerCase().startsWith(`spoiler_`)) name = name.slice(`spoiler_`.length)
-
-                    let date = null, datestr = null;
-
-                    if(name.startsWith(`VRChat_`) || name.startsWith(`vrchat_`)) {
-                        if(name.split(`_`).length === 4 && !name.includes(`(`) && !name.split(`_`).slice(-1)[0].includes(`x`)) name = name.split(`_`).slice(1).join(`_`)
-                        const day = name.split(`_`).slice(1, 2)[0].split(`-`);
-                        const time = name.split(`_`).slice(2, 3)[0].split(`.`)[0].split(`-`);
-                        datestr = `${day.map(s => s.split(`.`)[0]).join(`-`)}T${time.map(s => s.split(`.`)[0]).join(`:`)}`
-                        date = (new Date(datestr)).getTime()
-                    } else if(name[14] == `_`) {
-                        const yr = name.slice(0, 4), month = name.slice(4, 6), day = name.slice(6, 8), h = name.slice(8, 10), m = name.slice(10, 12), s = name.slice(12, 14);
-                        datestr = `${yr}-${month}-${day.split(`.`)[0]}T${h}:${m}:${s.split(`.`)[0]}`
-                        date = (new Date(datestr)).getTime()
-                    } else if(name[21] == `_`) {
-                        name = name.slice(21 - 14);
-                        const yr = name.slice(0, 4), month = name.slice(4, 6), day = name.slice(6, 8), h = name.slice(8, 10), m = name.slice(10, 12), s = name.slice(12, 14);
-                        datestr = `${yr}-${month}-${day.split(`.`)[0]}T${h}:${m}:${s.split(`.`)[0]}`
-                        date = (new Date(datestr)).getTime()
-                    };
-
-                    if(!date) console.log(dir.split(`/`).slice(-1)[0], datestr)
-
-                    const fd = fs.openSync(dir), fstat = fs.fstatSync(fd);
-                    const ms = date || Object.entries(fstat).filter(o => o[0].endsWith(`Ms`)).map(o => o[1]).sort()[0]
-                    const utc = time(ms).utc;
-
-                    const cachedImages = fs.readdirSync(`./cache/`)
-
-                    const cachedImage = require('../util/getCacheName')(dir.split(`/`).slice(2).join(`/`));
-
-                    //console.log(`${dir.split(`/`).slice(-1)[0]} as ${cachedImage} exists? ${cachedImages.indexOf(cachedImage) != -1 ? true : false}`)
-    
-                    return {
-                        createdAt: { ms, utc },
-                        name: dir.split(`/`).slice(-1)[0],
-                        location: dir.split(`/`).slice(2).join(`/`),
-                        type: dir.split(`/`).slice(-1)[0].split(`.`).slice(-1)[0],
-                        cachedImage: {
-                            file: cachedImage,
-                            exists: cachedImages.indexOf(cachedImage) != -1 ? true : false
-                        }
-                    }
-                } catch(e2) {
-                    console.warn(`Coudln't read directory OR file:\n- Directory: ${e}\n- File: ${e2}\nReturning null.`);
-                    return null;
-                }
-            }
+        if(currentFileStructure && Object.keys(fileStructure || {}).length === 0) {
+            console.log(`waiting`)
+            await currentFileStructure
         }
-        
-        const f = fs.readdirSync(`./files`).map(f => parseDir(`./files/${f}`));
-
-        res.send(f)
+        res.send(fileStructure)
     }
-}
+};
--- handlers/8-getYears.js
+++ handlers/8-getYears.js
@@ -3,53 +3,87 @@ const time = require(`../util/time`);
 
 let cached = null;
 
+const cacheNow = () => new Promise(async res => {
+    let toBeCached = {};
+
+    console.log(`caching years`);
+
+    let progress = 0;
+
+    const parseFiles = (d) => new Promise(async r => {
+        if(d.files && d.files.length > 0) {
+            for (i in d.files) {
+                let f = d.files[i];
+
+                progress = i/d.files.length
+
+                if(f) {
+                    if(f.type == `directory`) {
+                        //console.log(`parsing ${f.location} as directory`)
+                        parseFiles(f)
+                    } else {
+                        //console.log(`parsing ${f.location} as file`)
+                        const year = `${f.createdAt.utc.date.year}`;
+                        if(!toBeCached[year]) toBeCached[year] = {};
+                        
+                        const month = `${f.createdAt.utc.date.month}`;
+                        if(!toBeCached[year][month]) toBeCached[year][month] = [];
+
+                        toBeCached[year][month].push(Object.assign({}, f, {
+                            createdAt: f.createdAt.ms,
+                            year, month
+                        }))
+                    };
+                    await new Promise(r2 => setTimeout(r2, 5))
+                };
+            }
+        }; r()
+    })
+
+    const fileStructure = await new Promise(async resp => require(`./8-fileStructure`).func({ }, { send: resp, }));
+    
+    for(f of fileStructure) {
+        let timer = setInterval(() => console.log(`years parsing progress: ${Math.round(progress*100)}%`), 3000)
+        await parseFiles(f);
+        clearInterval(timer);
+        await new Promise(r => setTimeout(r, 10))
+    }
+
+    cached = toBeCached
+
+    console.log(`created cached years obj`);
+    res(cached)
+});
+
+let currentFileStructure = null;
+
+const timer = async () => {
+    while(true) {
+        await new Promise(async r => {
+            currentFileStructure = cacheNow();
+
+            await currentFileStructure;
+
+            currentFileStructure = null;
+
+            console.log(`caching years completed!`)
+
+            setTimeout(r, 60000);
+        })
+    }
+};
+
+timer();
+
 module.exports = {
     path: `/getYears`,
     func: async (req, res) => {
-        let files = {};
+        if(currentFileStructure && !cached) await currentFileStructure;
 
-        if(cached) {
-            files = cached
-        } else {
-            const parseFiles = (d) => {
-                if(d.files && d.files.length > 0) {
-                    d.files.forEach(f => {
-                        if(f) {
-                            if(f.type == `directory`) {
-                                parseFiles(f)
-                            } else {
-                                const year = `${f.createdAt.utc.date.year}`;
-                                if(!files[year]) files[year] = {};
-                                
-                                const month = `${f.createdAt.utc.date.month}`;
-                                if(!files[year][month]) files[year][month] = [];
-        
-                                files[year][month].push(Object.assign({}, f, {
-                                    createdAt: f.createdAt.ms,
-                                    year, month
-                                }))
-                            }
-                        }
-                    })
-                }
-            }
-    
-            const fileStructure = await new Promise(async resp => require(`./8-fileStructure`).func(req, Object.assign({}, res, { send: resp })));
-    
-            fileStructure.forEach(parseFiles);
+        const fileList = Object.assign([], ...Object.values(cached).map(o => Object.values(o)[0]))
 
-            cached = files;
-            console.log(`created cached years obj`)
-            setTimeout(() => {
-                console.log(`deleting cached years obj`)
-                cached = null;
-            }, 60000)
-        };
+        cached.getFile = (str) => fileList.find(o => o.name == str || o.location == str);
 
-        const fileList = Object.assign([], ...Object.values(files).map(o => Object.values(o)[0]))
-
-        files.getFile = (str) => fileList.find(o => o.name == str || o.location == str);
-
-        res.send(files)
+        res.send(cached)
     }
 }
