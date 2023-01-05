3c1740524b9fe3b3b61e4b4871a72fd9130518ef
Syl <hi@sylviiu.dev>
2023-01-04 18:23:56 -0600

    don't log when requests are sent

--- handlers/9-main.js
+++ handlers/9-main.js
@@ -14,22 +14,22 @@ module.exports = {
         const rawFile = files.getFile(req.params.path)
         
         if(rawFile) {
-            console.log(`Sending raw file -- raw path was given!`);
+            //console.log(`Sending raw file -- raw path was given!`);
             res.sendFile(__dirname.split(`/`).slice(0, -1).join(`/`) + `/files/` + rawFile.location)
         } else if(files[args[0]] && files[args[0]][args[1]] && files[args[0]][args[1]].find(o => o.name == args.slice(-1)[0])) {
-            console.log(`Sending processed file!`)
+            //console.log(`Sending processed file!`)
             res.sendFile(__dirname.split(`/`).slice(0, -1).join(`/`) + `/files/` + files[args[0]][args[1]].find(o => o.name == args.slice(-1)[0]).location)
         } else if(files[args[0]] && !files[args[0]][args[1]]) {
-            console.log(`Sending year directory of ${args[0]}`)
+            //console.log(`Sending year directory of ${args[0]}`)
             res.send(files[args[0]])
         } else if(files[args[0]] && files[args[0]][args[1]]) {
-            console.log(`Sending month directory of ${args[0]}/${args[1]}`)
+            //console.log(`Sending month directory of ${args[0]}/${args[1]}`)
             res.send(files[args[0]][args[1]])
         } else if(!args[0] && (args.length === 0 || args.length === 1)) {
-            console.log(`Sending root dir`)
+            //console.log(`Sending root dir`)
             res.send(files)
         } else {
-            console.log(`Sending absolutely nothing.`)
+            //console.log(`Sending absolutely nothing.`)
             res.send({})
         }
     }
